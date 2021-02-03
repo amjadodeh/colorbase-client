48b54e3dde6d2515fb8f27d9597dd8cbd117412d
amjad <aao001056485@gmail.com>
2021-02-03 12:01:37 -0500

    added BrowserPalettesPage component

---
+++ src/BrowsePalettesPage/BrowsePalettesPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PaletteList from '../PaletteList/PaletteList';
+
+export class BrowsePalettesPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default BrowsePalettesPage;
